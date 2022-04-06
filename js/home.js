f227f1407651c421fa7c6b1659e6835369e79ae5
Asmar <asmerhamidli@gmail.com>
2022-04-07 02:18:20 +0400

    main

---
+++ js/home.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  
