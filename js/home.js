9f2304a3f803470cb2be3def921ff90014480961
Asmar <asmerhamidli@gmail.com>
2022-04-09 01:49:06 +0400

    main

--- js/home.js
+++ js/home.js
@@ -1,12 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
   
 
 var btnContainer = document.getElementById("myDIV");
